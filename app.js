639c25efa72d6878b6a3a8ce3f813b1704a8ff8e
Panayot Angarev <pancho.angarev@gmail.com>
2019-01-06 19:31:31 +0200

    Add demage and  alert

--- app.js
+++ app.js
@@ -10,6 +10,36 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function () {
+
+            var damage = Math.max(Math.floor(Math.random() * 10) + 1, 3); //between min = 3 and max=10
+            this.monsterHealth -= damage;
+
+            if (this.monsterHealth <= 0) {
+                alert("You won!");
+                this.gameIsRunning = false;
+                return;
+            }
+            
+
+            damage = Math.max(Math.floor(Math.random() * 12) + 1, 5); //between min = 5 and max=12
+            this.playerHealth -= damage;
+
+            if (this.playerHealth <= 0) {
+                alert("The monster won!");
+                this.gameIsRunning = false;
+                return;
+            }
+        },
+        specilAttack: function () {
+            
+        },
+        heal: function () {
+            
+        },
+        giveUp: function () {
+            
         }
     },
 });
